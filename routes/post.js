1c8c2a4dfcf00195693f5497481132a704f42acc
Maxime Stefani <max341@free.fr>
2022-03-28 16:48:58 +0200

    test

--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
         cb(null,'./public/images/upload/');
     },
     filename: function(req,file,cb){
-        cb(null, new Date().toISOString() + file.originalname)
+        cb(null, new Date().toDateString() + file.originalname)
     }
 });
 
